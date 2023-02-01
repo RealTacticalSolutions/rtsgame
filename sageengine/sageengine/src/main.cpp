3aa950f26604c9609197864aecffca1250ef1691
StefanNHL <admin>
2023-02-01 15:35:24 +0100

    finished instance

--- sageengine/sageengine/src/main.cpp
+++ sageengine/sageengine/src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -25,6 +26,8 @@ public:
     }
 
 private:
+    VkInstance instance;
+
     void initWindow() {
         glfwInit();
 
@@ -34,7 +37,61 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
     void initVulkan() {
+        createInstance();
+    }
+    void createInstance()
+    {
+        // Creates usefull information for the driver
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
+        // Tells Vulkan driver which global extensions and validation layers we want to use, global means they apply to the whole program and not specific device 
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        checkExtensions();
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    void checkExtensions()
+    {
+        uint32_t extensionCount = 0;
+
+        // You can request just the number of extensions by leaving the latter parameter empty
+        //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+        //As a challenge, try to create a function that checks if all of the extensions returned by glfwGetRequiredInstanceExtensions() are included in the supported extensions list.
     }
 
     void mainLoop() {
@@ -44,6 +101,8 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
