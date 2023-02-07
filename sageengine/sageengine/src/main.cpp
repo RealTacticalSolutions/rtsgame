2f6937e5ba8e794039271632e5a8fcea9706463f
StefanNHL <admin>
2023-02-07 14:07:54 +0100

    added 3d mike

--- sageengine/sageengine/src/main.cpp
+++ sageengine/sageengine/src/main.cpp
@@ -29,8 +29,8 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "models/monkey.obj";
-const std::string TEXTURE_PATH = "textures/monkey.png";
+const std::string MODEL_PATH = "models/mike.obj";
+const std::string TEXTURE_PATH = "textures/mike.png";
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
@@ -1318,8 +1318,8 @@ private:
 
         UniformBufferObject ubo{};
         ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
+        ubo.view = glm::lookAt(glm::vec3(2.0f, -20.0f, 5.0f), glm::vec3(0.0f, 0.0f, 4.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 100.0f);
         ubo.proj[1][1] *= -1;
 
         memcpy(uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
