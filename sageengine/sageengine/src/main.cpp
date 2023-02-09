f022b7b1060302fbab90744d335c2460202e1f0a
StefanNHL <admin>
2023-02-09 14:38:56 +0100

    forgot one variable

--- sageengine/sageengine/src/main.cpp
+++ sageengine/sageengine/src/main.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 
 int main() {
-    std::unique_ptr<application> program= std::make_unique<program>();
+    std::unique_ptr<application> program = std::make_unique<application>();
 
     program->run();
 
