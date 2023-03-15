98f0f1bbd58dfea9333a656e37a5d3a99c6928d1
robinhoogterp <robinhoogterp.mail@gmail.com>
2023-03-15 12:31:29 +0100

    Added a bit of documentation to input

--- sageengine/sageengine/include/inputsystem.h
+++ sageengine/sageengine/include/inputsystem.h
@@ -14,11 +14,39 @@ public:
 		initCallback();
 	};
 
+	/*
+	*	@brief a function that returns true if a given key is pressed
+	* 
+	*	@param key the key to check for
+	*	@returns returns if key is pressed or not
+	*/
 	bool keyPressed(int key);
+
+	/*
+	*	@brief a function that returns true if a given key is released
+	*
+	*	@param key the key to check for
+	*	@returns returns if key is released this frame
+	*/
 	bool keyUp(int key);
+
+	/*
+	*	@brief a function that returns true if a given key is pressed first this frame
+	*
+	*	@param key the key to check for
+	*	@returns returns if key is pressed first this frame
+	*/
 	bool keyDown(int key);
 	
+
+	/** updates the keystate for each key every frame */
 	void updateInput();
 
+
+	/*
+	*	@brief a function that gets the cursor position in relation to the window
+	*
+	*	@returns returns a vector2 that holds the x and y position of the cursor in pixels from away from the window origin
+	*/
 	glm::vec2 getCursorPos();
 };
