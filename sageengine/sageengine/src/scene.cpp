32dd2e85e38ebd218c15246b1f6a85fd962f7b2f
StefanNHL <admin>
2023-04-03 14:20:53 +0200

    push for fix

--- sageengine/sageengine/src/scene.cpp
+++ sageengine/sageengine/src/scene.cpp
@@ -40,7 +40,21 @@ void Scene::instantiateCar(BluePrint bluePrint, glm::mat4 transform, glm::vec3 c
 void Scene::removeObject(int index)
 {
 	if (gameObjects.size() > index) {
+		int removeid = gameObjects[index]->instanceId;
+		int blueprintindex = 2; // Todo find a proper way to get blueprint id
 		gameObjects[index]->renderObject->instanceCount -= 1;
+		gameObjects[index]->instanceId -= 1;
 		gameObjects.erase(gameObjects.begin() + index);
-	}
+
+		int arraylenght = gameObjects[index]->renderObject->instanceCount;
+		for (int i = removeid; i < arraylenght - 1; i++) {
+			renderObjects[blueprintindex].renderprops.instances[i] = renderObjects[blueprintindex].renderprops.instances[i +1];
+			renderObjects[blueprintindex].renderprops.color[i] = renderObjects[blueprintindex].renderprops.color[i + 1];
+		}
+			// set last new index to 0
+			renderObjects[blueprintindex].renderprops.instances[arraylenght - 1] = glm::mat4(0);
+			renderObjects[blueprintindex].renderprops.color[arraylenght - 1] = glm::vec4(0.0f,0.0f,0.0f,0.0f);
+				
+		}
+	
 }
