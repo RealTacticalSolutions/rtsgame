d6c4a68cdb263d1ef85973c474071002e4f8f99c
robinhoogterp <robinhoogterp.mail@gmail.com>
2023-05-02 12:04:48 +0200

    improved on all fronts

--- TrafficController/TrafficController/Program.cs
+++ TrafficController/TrafficController/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 public enum lightstatus
 {
@@ -13,8 +14,6 @@ public enum lightstatus
     green
 }
 
-
-
 public class Program {
 
 
@@ -22,6 +21,19 @@ public class Program {
     static IPEndPoint ipEndPoint;
     static Socket listener;
     static Socket socket;
+    static TrafficLight[] trafficLights;
+
+
+    //Setups declaration
+
+    static double[] setup1 = { 1.1, 2.1, 32.2, 35.2, 36.1, 37.1, 37.2 };
+    static double[] setup2 = { 5.1, 6.1, 31.1, 31.2, 35.1, 35.2, 37.1, 37.2};
+    static double[] setup3 = { 7.1, 8.1, 9.1, 10.1, 31.1, 31.2};
+    static double[] setup4 = { 11.1, 12.1, 26.1, 31.1,31.2, 35.1, 35.2, 36.1, 36.2, 38.1, 38.2, 86.1};
+    static double[] setup5 = { 6.1, 7.1, 22.0, 28.1, 31.1, 31.2, 32.1, 32.2, 37.1, 37.2, 38.1, 38.2, 88.1};
+    static double[] setup6 = { 7.1, 8.1, 28.1, 37.1 ,37.2, 38.1, 38.2, 42, 88.1 };
+
+    static List<double[]> setups = new();
 
     public static void InitSocket()
     {
@@ -46,21 +58,20 @@ public class Program {
 
     public static void Tick()
     {
-        int setup = 0;
+        int currentSetup = -1;
+        Stopwatch sw = Stopwatch.StartNew();
+        int trafficLightCount = trafficLights.Length;
 
-        double[] lightIds = { 1.1, 2.1, 5.1, 6.1, 7.1, 8.1, 9.1, 10.1, 11.1, 12.1, 35.1, 35.2, 36.1, 36.2, 37.1, 37.2, 38.1, 38.2, 31.1, 31.2, 32.1, 32.2, 86.1, 26.1, 88.1, 28.1, 22.0, 42.0 };
+        TrafficlightInfo[] trafficlightStatusses = new TrafficlightInfo[trafficLights.Length];
 
-        TrafficlightInfo[] trafficlightInfos = new TrafficlightInfo[lightIds.Length];
-
-        for (int i = 0; i < lightIds.Length; i++)
+        for (int i = 0; i < trafficLights.Length; i++)
         {
-            trafficlightInfos[i] = new TrafficlightInfo()
+            trafficlightStatusses[i] = new TrafficlightInfo()
             {
-                id = lightIds[i],
+                id = trafficLights[i].id,
                 status = lightstatus.red
             };
         }
-
 
         int sendResult;
         int receiveResult;
@@ -79,15 +90,16 @@ public class Program {
 
                 int bytesRead = socket.Receive(buffer);
                 response = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                response = response.TrimEnd('\n');
                 buffer = new byte[8196];
                 Console.WriteLine($"received:  {response}");
 
                 var JsonResponse = JsonSerializer.Deserialize<TrafficInfo[]>(response);
 
-                foreach (TrafficInfo trafficInfo in JsonResponse)
+                for (int i = 0; i < trafficLightCount; i++)
                 {
-                    Console.WriteLine($"LightId: {trafficInfo.id} Weight: {trafficInfo.weight} ");
-
+                    trafficLights[i].weight = JsonResponse[i].weight;
+                    Console.WriteLine($"LightId: {JsonResponse[i].id} Weight: {JsonResponse[i].weight} ");
                 }
             }
             catch (Exception e)
@@ -96,8 +108,13 @@ public class Program {
             }
             try
             {
+                //sends the trafficlight data
+                for(int i = 0; i < trafficLightCount; i++)
+                {
+                    trafficlightStatusses[i].status = trafficLights[i].status;
+                }
 
-                string message = JsonSerializer.Serialize<TrafficlightInfo[]>(trafficlightInfos);
+                string message = JsonSerializer.Serialize<TrafficlightInfo[]>(trafficlightStatusses);
                 message += '\n';
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
                 if (socket.Connected)
@@ -106,38 +123,7 @@ public class Program {
                     sendResult = socket.Send(data);
                 }
 
-                foreach (TrafficlightInfo info in trafficlightInfos)
-                {
-                    info.status = lightstatus.red;
-                }
 
-                switch (setup)
-                {
-                    case 0:
-                        trafficlightInfos[0].status = lightstatus.green;
-                        trafficlightInfos[1].status = lightstatus.green;
-                        trafficlightInfos[10].status = lightstatus.green;
-                        trafficlightInfos[27].status = lightstatus.green;
-                        setup = 1;
-                        break;
-                    case 1:
-                        trafficlightInfos[2].status = lightstatus.green;
-                        trafficlightInfos[3].status = lightstatus.green;
-                        setup = 2;
-                        break;
-                    case 2:
-                        trafficlightInfos[4].status = lightstatus.green;
-                        trafficlightInfos[5].status = lightstatus.green;
-                        trafficlightInfos[6].status = lightstatus.green;
-                        setup = 3;
-                        break;
-                    case 3:
-                        trafficlightInfos[7].status = lightstatus.green;
-                        trafficlightInfos[8].status = lightstatus.green;
-                        trafficlightInfos[9].status = lightstatus.green;
-                        setup = 0;
-                        break;
-                }
             }
             catch (Exception e)
             {
@@ -147,7 +133,46 @@ public class Program {
                 }
                 Console.WriteLine("Sending failed");
                 Console.WriteLine("ERROR: " + e);
+            }
 
+            //if all trafficlights are clear generate new setup
+            if (currentSetup == -1)
+            {
+                int maxWeight = 0;
+
+                for (int i = 0; i < setups.Count; i++)
+                {
+                    int totalWeight = 0;
+                    for (int j = 0; j < setups[i].Length; j++)
+                    {
+                        totalWeight += trafficLights.First(light => light.id == setups[i][j]).weight;
+                    }
+                    if (totalWeight > maxWeight)
+                    {
+                        currentSetup = i;
+                    }
+                }
+            }
+
+            //set trafficlight statusses to correct status using the elapsed time
+            for (int i = 0; i < setups[currentSetup].Length; i++)
+            {
+                TrafficLight light = trafficLights.First(light => light.id == setups[currentSetup][i]);
+
+                if (sw.ElapsedMilliseconds > 3000)
+                {
+                    light.status = lightstatus.orange;
+                }
+                else if (sw.ElapsedMilliseconds > 5000)
+                {
+                    light.status = lightstatus.red;
+                    sw.Restart();
+                    currentSetup = -1;
+                }
+                else
+                {
+                    light.status = lightstatus.green;
+                }
             }
         }
         socket.Shutdown(SocketShutdown.Both);
@@ -156,11 +181,30 @@ public class Program {
 
     public static void Main()
     {
+        double[] lightIds = { 1.1, 2.1, 5.1, 6.1, 7.1, 8.1, 9.1, 10.1, 11.1, 12.1, 35.1, 35.2, 36.1, 36.2, 37.1, 37.2, 38.1, 38.2, 31.1, 31.2, 32.1, 32.2, 86.1, 26.1, 88.1, 28.1, 22.0, 42.0 };
+        trafficLights = new TrafficLight[lightIds.Length];
+
+        for(int i = 0; i < lightIds.Length; i++)
+        {
+            trafficLights[i] = new TrafficLight();
+            trafficLights[i].id = lightIds[i];
+            trafficLights[i].weight = 0;
+            trafficLights[i].status = lightstatus.red;
+        }
+
+        setups.Add(setup1);
+        setups.Add(setup2);
+        setups.Add(setup3);
+        setups.Add(setup4);
+        setups.Add(setup5);
+        setups.Add(setup6);
 
         InitSocket();
         Console.WriteLine("connection succeeded");
 
         Tick();
+
+        
 
         while (true)
         {
@@ -192,6 +236,15 @@ public class TrafficInfo
 public class TrafficlightInfo
 {
     public double id { get; set; }
+
+    public lightstatus status { get; set; }
+}
+
+public class TrafficLight
+{
+    public double id { get; set; }
+
+    public int weight { get; set; }
 
     public lightstatus status { get; set; }
 }
